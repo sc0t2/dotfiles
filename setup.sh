6f624c095dc2cc1abfe5c4944ab737307bd04fed
scottmurs <scott.myers622@gmail.com>
2019-10-04 21:11:17 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# symlink dotfile at $1 to destination at $2
+function dotfile {
+	SRC=$1
+	DEST=$2
+	ln -s $(readlink -f $SRC) $DEST
+}
+
+# make nvim config directory if it doesn't exist
+if [ ! -d "~/.config/nvim" ]; then
+	mkdir -p ~/.config/nvim
+fi
+
+dotfile ./bash/.bash_aliases ~/.bash_aliases
+dotfile ./nvim/init.vim ~/.config/nvim/init.vim
+dotfile ./tmux/.tmux.conf ~/.tmux.conf
+dotfile ./vim/.vimrc ~/.vimrc
+
